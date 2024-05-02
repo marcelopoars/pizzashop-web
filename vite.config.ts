613ad1deca6f3bfdd694b724c2b54ec237500a1e
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2024-05-02 13:55:40 -0300

    chore: install vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import path from 'node:path'
+
+import react from '@vitejs/plugin-react'
+import { defineConfig, UserConfig } from 'vite'
+import { InlineConfig } from 'vitest'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,4 +13,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  test: {
+    globals: true,
+  },
+} as UserConfig & {
+  test: InlineConfig
 })
